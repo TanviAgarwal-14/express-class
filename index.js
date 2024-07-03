c1bfac45f0c140329decf83f14c12ea252e2ca2e
TanviAgarwal-14 <tanvi.23bcs10143@ms.sst.scaler.com>
2024-07-03 10:47:52 +0530

    More Code

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express= require('express');
 const app= express();
 
+app.use(express.json());
+
 let courses= [
     {id: 1, name: "java"},
     {id: 2, name: "javascript"},
@@ -10,6 +12,16 @@ let courses= [
 app.get('/courses', (req, res) =>{
     res.json(courses);
 })
+
+app. post('/courses', (req, res) =>{
+    console.log(req.body);
+    let singleCourse= {
+        id: courses.length+1,
+        name: req.body.name
+    }
+    courses.push(singleCourse);
+    res.send(courses);
+}) 
 
 app.listen('3000', () => {
     console.log("server started")
